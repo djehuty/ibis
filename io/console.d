8c33aef225fc585129b75cbf1427894174693167
wilkie <wilkie05@gmail.com>
2012-06-22 22:52:31 -0400

    Adds foregroundColor and backgroundColor implementations to Console for Linux platforms.

--- io/console.d
+++ io/console.d
@@ -2,6 +2,8 @@ module io.console;
 
 import io.stream;
 
+import drawing.color;
+
 version(Windows) {
 }
 else version(linux) {
@@ -13,9 +15,98 @@ else {
 
 class Console {
 private:
+  Color _fg;
+  Color _bg;
+
   Stream _input;
   Stream _output;
   Stream _stream;
+
+  int _toNearestConsoleColor(Color clr) {
+    // 16 colors on console
+    // For each channel, it can be 00, 88, or ff
+    // That is, something mid range
+
+    int nearRed, nearGreen, nearBlue;
+    int ret;
+
+    nearRed = cast(int)((clr.red * 3.0) + 0.5);
+    nearGreen = cast(int)((clr.green * 3.0) + 0.5);
+    nearBlue = cast(int)((clr.blue * 3.0) + 0.5);
+
+    if ((nearRed == nearGreen) && (nearGreen == nearBlue)) {
+      // gray
+      if (clr.red < (Color.DarkGray.red / 2.0)) {
+        // Closer to black
+        ret = 0;
+      }
+      else if (clr.red < ((Color.Gray.red - Color.DarkGray.red) / 2.0) + Color.DarkGray.red) {
+        // Closer to dark gray
+        ret = 8;
+      }
+      else if (clr.red < ((Color.White.red - Color.Gray.red) / 2.0) + Color.Gray.red) {
+        // Closer to light gray
+        ret = 7;
+      }
+      else {
+        // Closer to white
+        ret = 15;
+      }
+    }
+    else {
+      // Nearest color match
+      static int[][] translations = [
+        [1,0,0], // 1, Dark Red
+        [0,1,0], // 2, Dark Green
+        [1,1,0], // 3, Dark Yellow
+        [0,0,1], // 4, Dark Blue
+        [1,0,1], // 5, Dark Magenta
+        [0,1,1], // 6, Dark Cyan
+
+        [2,0,0], // 9, Dark Red
+        [0,2,0], // 10, Dark Green
+        [2,2,0], // 11, Dark Yellow
+        [0,0,2], // 12, Dark Blue
+        [2,0,2], // 13, Dark Magenta
+        [0,2,2], // 14, Dark Cyan
+      ];
+
+      double mindistance = 4 * 3;
+
+      foreach(size_t i, coord; translations) {
+        // Compare euclidian distance
+        double distance = 0.0;
+        double intermediate;
+
+        intermediate = coord[0] - nearRed;
+        intermediate *= intermediate;
+
+        distance += intermediate;
+
+        intermediate = coord[1] - nearGreen;
+        intermediate *= intermediate;
+
+        distance += intermediate;
+
+        intermediate = coord[2] - nearBlue;
+        intermediate *= intermediate;
+
+        distance += intermediate;
+
+        // Omitting square root, it is unnecessary for comparison
+        if (mindistance > distance) {
+          mindistance = distance;
+          ret = i;
+          ret++;
+          if (ret > 6) {
+            ret += 2;
+          }
+        }
+      }
+    }
+
+    return ret;
+  }
 
   ubyte[] _read(size_t length) {
     return null;
@@ -69,6 +160,38 @@ public:
 
   Stream stream() {
     return _stream;
+  }
+
+  Color foregroundColor() {
+    return _fg;
+  }
+
+  Color backgroundColor() {
+    return _bg;
+  }
+
+  void foregroundColor(Color value) {
+    _fg = value;
+
+    version(linux) {
+      int fgidx = _toNearestConsoleColor(value);
+      int bright = 0;
+      if (fgidx > 7) {
+        fgidx %= 8;
+        bright = 1;
+      }
+      printf("\x1B[%d;%dm\0".ptr, bright, 30 + fgidx);
+    }
+  }
+
+  void backgroundColor(Color value) {
+    _bg = value;
+
+    version(linux) {
+      int bgidx = _toNearestConsoleColor(value);
+      bgidx %= 8;
+      printf("\x1B[%dm\0".ptr, 40 + bgidx);
+    }
   }
 
   ubyte[] read(size_t length) {
