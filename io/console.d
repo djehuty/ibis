100f28bc07e0e72f58f128b75b23114e872e8c65
wilkie <wilkie05@gmail.com>
2012-06-26 19:08:47 -0400

    Adds support for 256 color terminals to Console in package Io.

--- io/console.d
+++ io/console.d
@@ -23,85 +23,79 @@ private:
   Stream _stream;
 
   int _toNearestConsoleColor(Color clr) {
-    // 16 colors on console
-    // For each channel, it can be 00, 88, or ff
-    // That is, something mid range
+    int ret = 0;
+    double mindistance = 100;
 
-    int nearRed, nearGreen, nearBlue;
-    int ret;
+    // 16 colors or 256?
 
-    nearRed = cast(int)((clr.red * 3.0) + 0.5);
-    nearGreen = cast(int)((clr.green * 3.0) + 0.5);
-    nearBlue = cast(int)((clr.blue * 3.0) + 0.5);
+    static int[] colors = [
+      0x000000, 0xAA0000, 0x00AA00, 0xAA5500, 0x0000AA, 0xAA00AA, 0x00AAAA,
+      0xAAAAAA, 0x555555, 0xFF5555, 0x55FF55, 0xFFFF55, 0x5555FF, 0xFF55FF,
+      0x55FFFF, 0xFFFFFF, 0x000000, 0x00005f, 0x000087, 0x0000af, 0x0000d7,
+      0x0000ff, 0x005f00, 0x005f5f, 0x005f87, 0x005faf, 0x005fd7, 0x005fff,
+      0x008700, 0x00875f, 0x008787, 0x0087af, 0x0087d7, 0x0087ff, 0x00af00,
+      0x00af5f, 0x00af87, 0x00afaf, 0x00afd7, 0x00afff, 0x00d700, 0x00d75f,
+      0x00d787, 0x00d7af, 0x00d7d7, 0x00d7ff, 0x00ff00, 0x00ff5f, 0x00ff87,
+      0x00ffaf, 0x00ffd7, 0x00ffff, 0x5f0000, 0x5f005f, 0x5f0087, 0x5f00af,
+      0x5f00d7, 0x5f00ff, 0x5f5f00, 0x5f5f5f, 0x5f5f87, 0x5f5faf, 0x5f5fd7,
+      0x5f5fff, 0x5f8700, 0x5f875f, 0x5f8787, 0x5f87af, 0x5f87d7, 0x5f87ff,
+      0x5faf00, 0x5faf5f, 0x5faf87, 0x5fafaf, 0x5fafd7, 0x5fafff, 0x5fd700,
+      0x5fd75f, 0x5fd787, 0x5fd7af, 0x5fd7d7, 0x5fd7ff, 0x5fff00, 0x5fff5f,
+      0x5fff87, 0x5fffaf, 0x5fffd7, 0x5fffff, 0x870000, 0x87005f, 0x870087,
+      0x8700af, 0x8700d7, 0x8700ff, 0x875f00, 0x875f5f, 0x875f87, 0x875faf,
+      0x875fd7, 0x875fff, 0x878700, 0x87875f, 0x878787, 0x8787af, 0x8787d7,
+      0x8787ff, 0x87af00, 0x87af5f, 0x87af87, 0x87afaf, 0x87afd7, 0x87afff,
+      0x87d700, 0x87d75f, 0x87d787, 0x87d7af, 0x87d7d7, 0x87d7ff, 0x87ff00,
+      0x87ff5f, 0x87ff87, 0x87ffaf, 0x87ffd7, 0x87ffff, 0xaf0000, 0xaf005f,
+      0xaf0087, 0xaf00af, 0xaf00d7, 0xaf00ff, 0xaf5f00, 0xaf5f5f, 0xaf5f87,
+      0xaf5faf, 0xaf5fd7, 0xaf5fff, 0xaf8700, 0xaf875f, 0xaf8787, 0xaf87af,
+      0xaf87d7, 0xaf87ff, 0xafaf00, 0xafaf5f, 0xafaf87, 0xafafaf, 0xafafd7,
+      0xafafff, 0xafd700, 0xafd75f, 0xafd787, 0xafd7af, 0xafd7d7, 0xafd7ff,
+      0xafff00, 0xafff5f, 0xafff87, 0xafffaf, 0xafffd7, 0xafffff, 0xd70000,
+      0xd7005f, 0xd70087, 0xd700af, 0xd700d7, 0xd700ff, 0xd75f00, 0xd75f5f,
+      0xd75f87, 0xd75faf, 0xd75fd7, 0xd75fff, 0xd78700, 0xd7875f, 0xd78787,
+      0xd787af, 0xd787d7, 0xd787ff, 0xd7af00, 0xd7af5f, 0xd7af87, 0xd7afaf,
+      0xd7afd7, 0xd7afff, 0xd7d700, 0xd7d75f, 0xd7d787, 0xd7d7af, 0xd7d7d7,
+      0xd7d7ff, 0xd7ff00, 0xd7ff5f, 0xd7ff87, 0xd7ffaf, 0xd7ffd7, 0xd7ffff,
+      0xff0000, 0xff005f, 0xff0087, 0xff00af, 0xff00d7, 0xff00ff, 0xff5f00,
+      0xff5f5f, 0xff5f87, 0xff5faf, 0xff5fd7, 0xff5fff, 0xff8700, 0xff875f,
+      0xff8787, 0xff87af, 0xff87d7, 0xff87ff, 0xffaf00, 0xffaf5f, 0xffaf87,
+      0xffafaf, 0xffafd7, 0xffafff, 0xffd700, 0xffd75f, 0xffd787, 0xffd7af,
+      0xffd7d7, 0xffd7ff, 0xffff00, 0xffff5f, 0xffff87, 0xffffaf, 0xffffd7,
+      0xffffff, 0x080808, 0x121212, 0x1c1c1c, 0x262626, 0x303030, 0x3a3a3a,
+      0x444444, 0x4e4e4e, 0x585858, 0x626262, 0x6c6c6c, 0x767676, 0x808080,
+      0x8a8a8a, 0x949494, 0x9e9e9e, 0xa8a8a8, 0xb2b2b2, 0xbcbcbc, 0xc6c6c6,
+      0xd0d0d0, 0xdadada, 0xe4e4e4, 0xeeeeee
+    ];
 
-    if ((nearRed == nearGreen) && (nearGreen == nearBlue)) {
-      // gray
-      if (clr.red < (Color.DarkGray.red / 2.0)) {
-        // Closer to black
-        ret = 0;
-      }
-      else if (clr.red < ((Color.Gray.red - Color.DarkGray.red) / 2.0) + Color.DarkGray.red) {
-        // Closer to dark gray
-        ret = 8;
-      }
-      else if (clr.red < ((Color.White.red - Color.Gray.red) / 2.0) + Color.Gray.red) {
-        // Closer to light gray
-        ret = 7;
-      }
-      else {
-        // Closer to white
-        ret = 15;
-      }
-    }
-    else {
-      // Nearest color match
-      static int[][] translations = [
-        [1,0,0], // 1, Dark Red
-        [0,1,0], // 2, Dark Green
-        [1,1,0], // 3, Dark Yellow
-        [0,0,1], // 4, Dark Blue
-        [1,0,1], // 5, Dark Magenta
-        [0,1,1], // 6, Dark Cyan
+    foreach(size_t idx, comparedColor; colors) {
+      double red   = cast(double)((comparedColor >> 16) & 0xff) / cast(double)0xff;
+      double green = cast(double)((comparedColor >>  8) & 0xff) / cast(double)0xff;
+      double blue  = cast(double)((comparedColor >>  0) & 0xff) / cast(double)0xff;
 
-        [2,0,0], // 9, Dark Red
-        [0,2,0], // 10, Dark Green
-        [2,2,0], // 11, Dark Yellow
-        [0,0,2], // 12, Dark Blue
-        [2,0,2], // 13, Dark Magenta
-        [0,2,2], // 14, Dark Cyan
-      ];
+      // Compare euclidian distance
+      double distance = 0.0;
+      double intermediate;
 
-      double mindistance = 4 * 3;
+      intermediate  = red - clr.red;
+      intermediate *= intermediate;
 
-      foreach(size_t i, coord; translations) {
-        // Compare euclidian distance
-        double distance = 0.0;
-        double intermediate;
+      distance += intermediate;
 
-        intermediate = coord[0] - nearRed;
-        intermediate *= intermediate;
+      intermediate  = green - clr.green;
+      intermediate *= intermediate;
 
-        distance += intermediate;
+      distance += intermediate;
 
-        intermediate = coord[1] - nearGreen;
-        intermediate *= intermediate;
+      intermediate  = blue - clr.blue;
+      intermediate *= intermediate;
 
-        distance += intermediate;
+      distance += intermediate;
 
-        intermediate = coord[2] - nearBlue;
-        intermediate *= intermediate;
-
-        distance += intermediate;
-
-        // Omitting square root, it is unnecessary for comparison
-        if (mindistance > distance) {
-          mindistance = distance;
-          ret = i;
-          ret++;
-          if (ret > 6) {
-            ret += 2;
-          }
-        }
+      // Omitting square root, it is unnecessary for comparison
+      if (mindistance > distance) {
+        mindistance = distance;
+        ret = idx;
       }
     }
 
@@ -175,12 +169,17 @@ public:
 
     version(linux) {
       int fgidx = _toNearestConsoleColor(value);
-      int bright = 0;
-      if (fgidx > 7) {
-        fgidx %= 8;
-        bright = 1;
+      if (fgidx < 16) {
+        int bright = 0;
+        if (fgidx > 7) {
+          fgidx %= 8;
+          bright = 1;
+        }
+        printf("\x1B[%d;%dm\0".ptr, bright, 30 + fgidx);
       }
-      printf("\x1B[%d;%dm\0".ptr, bright, 30 + fgidx);
+      else {
+        printf("\x1B[38;5;%dm\0".ptr, fgidx);
+      }
     }
   }
 
@@ -189,8 +188,13 @@ public:
 
     version(linux) {
       int bgidx = _toNearestConsoleColor(value);
-      bgidx %= 8;
-      printf("\x1B[%dm\0".ptr, 40 + bgidx);
+      if (bgidx < 16) {
+        bgidx %= 8;
+        printf("\x1B[%dm\0".ptr, 40 + bgidx);
+      }
+      else {
+        printf("\x1B[48;5;%dm\0".ptr, bgidx);
+      }
     }
   }
 
